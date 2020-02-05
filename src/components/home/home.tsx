aee0b8fdaf1425ecbd6889776aabfd896efc5d6b
SJT00 <saadtaj96@gmail.com>
2020-02-05 16:52:13 -0500

    Centered login

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -5,10 +5,12 @@ import Form from "./../form/form";
 
 const Home = () => {
   return (
-    <div id="login">
-      <h1>Budgeting App</h1>
-      <h3>Please enter your credentials</h3>
-      <Form />
+    <div id="home">
+      <div id="login">
+        <h1>Budgeting App</h1>
+        <h3>Please enter your credentials</h3>
+        <Form />
+      </div>
     </div>
   );
 };
