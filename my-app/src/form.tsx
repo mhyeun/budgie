f6e3d52006c229b705711a8d006cb5e36d54e15c
mhyeun <mhyeun131@gmail.com>
2020-02-04 22:08:49 -0500

    Add form component

---
+++ my-app/src/form.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+const Form = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    
+    return (
+        <div>
+            <input 
+            name="username"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+            />
+            <input 
+            type="password"
+            name="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            />
+        </div>
+    )
+}
+
+export default Form;
