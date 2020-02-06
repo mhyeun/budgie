cf94d9b94dac0b8a52ee906acf15cd6a71984e7d
SJT00 <saadtaj96@gmail.com>
2020-02-05 22:19:22 -0500

    Added Ui, deleted dated files.

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import "./home.scss";
-
 import Form from "./../form/form";
 
 const Home = () => {
   return (
     <div id="home">
       <div id="login">
+        {/* <image src="../../icons/logo.svg" /> */}
         <h1>Budgeting App</h1>
         <h3>Please enter your credentials</h3>
         <Form />
