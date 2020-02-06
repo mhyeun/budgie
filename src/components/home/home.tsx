4857a6cde59fbe8b3a662a5846a1636cd86dd585
SJT00 <saadtaj96@gmail.com>
2020-02-05 22:54:52 -0500

    Added logo, and setup ts import defaults.

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import "./home.scss";
 import Form from "./../form/form";
+import logo from "./../../icons/login.svg";
 
 const Home = () => {
   return (
     <div id="home">
       <div id="login">
-        {/* <image src="../../icons/logo.svg" /> */}
+        <img id="logo" src={logo} />
         <h1>Budgeting App</h1>
         <h3>Please enter your credentials</h3>
         <Form />
