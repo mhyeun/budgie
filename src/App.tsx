5faa2185a76398ff3f658779e4053a325b19bfb3
Matthew Hyeun <matthewhyeun@Kevins-MBP.localdomain>
2020-02-06 15:08:47 -0500

    Add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import "./App.scss";
 
 import Home from "./components/home/home";
 import Dashboard from "./components/dashboard/dashboard";
+import NavBar from "./components/navbar/navbar";
 
 const App = () => {
   return (
     <div>
+      <NavBar />
       <Home />
       <Dashboard />
     </div>
