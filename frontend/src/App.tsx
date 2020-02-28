feab96543044c2e445f194040e2a22cd93fa62ee
SJT00 <saadtaj96@gmail.com>
2020-02-27 21:29:52 -0500

    Added router.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,24 @@
-import React from "react";
+import * as React from "react";
 import "./App.scss";
 
 import Home from "./components/home/home";
 import Dashboard from "./components/dashboard/dashboard";
 import NavBar from "./components/navbar/navbar";
+import history from "./history";
+import { Route, Router, BrowserRouter, Switch } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <NavBar />
-      <Home />
-      <Dashboard />
+      <BrowserRouter>
+        <Router history={history}>
+          <Switch>
+            <Route exact path="/" component={() => <Home />} />
+            <Route exact path="/dashboard" component={() => <Dashboard />} />
+          </Switch>
+        </Router>
+      </BrowserRouter>
     </div>
   );
 };
