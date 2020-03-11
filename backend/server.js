ea71c758e94977dd93cbcfa376bcf3674aae7bb3
mhyeun <mhyeun131@gmail.com>
2020-03-10 22:12:53 -0400

    Add comments, add models folder

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
+// imports
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
-
+const mongoose = require("mongoose");
 require("dotenv").config();
 
+// express server
 const app = express();
 const port = process.env.PORT || 5000;
+
+// middleware, express.json used to parse json
 app.use(cors());
 app.use(express.json());
 
+// database connection
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 
@@ -17,6 +21,7 @@ connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
 
+// start server
 app.listen(port, () => {
   console.log("Server is running on port: " + port);
 });
