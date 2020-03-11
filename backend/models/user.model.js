c6b6706de4b907888eab3e2365ea03baba4f51ca
mhyeun <mhyeun131@gmail.com>
2020-03-10 22:23:53 -0400

    Create models folder and user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// define the shape of a user within this collection
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 10
+    },
+}, {
+    timestamps: true,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
