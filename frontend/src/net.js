4b3bcc760cf1158ebb63e94a37c0c1c4bbd9fb54
mhyeun <mhyeun131@gmail.com>
2020-04-04 21:29:01 -0400

    Add authUser function in net.js

--- frontend/src/net.js
+++ frontend/src/net.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const bcrypt = require("bcryptjs");
 
 // saad using port 5000 for server
 const url = "https://localhost:5000";
@@ -37,5 +38,19 @@ export async function getUser(username) {
   } catch (err) {
     console.error(err);
     Promise.reject(err);
+  }
+}
+
+export async function authUser(username, password) {
+  try {
+    const res = await axios.get(`${url}/users/${username}`);
+    const passwordsMatch = await bcrypt.compare(password, res.data.password);
+    const usernamesMatch = res.data.username === username;
+    if (passwordsMatch && usernamesMatch) return true;
+    return false;
+  } catch (err) {
+    console.error(err);
+    Promise.reject(err);
+    return false;
   }
 }
