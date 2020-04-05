9a06a0992bec1fd157ebfd232f7e24ecacefa744
SJT00 <saadtaj96@gmail.com>
2020-04-04 23:33:21 -0400

    net changes

--- frontend/src/net.tsx
+++ frontend/src/net.tsx
@@ -2,13 +2,13 @@ const axios = require("axios");
 const bcrypt = require("bcryptjs");
 
 // saad using port 5000 for server
-const url = "https://localhost:5000";
+const url = "http://localhost:5000";
 
 export async function createUser(username: String, password: String) {
   try {
     const res = await axios.post(`${url}/users/add`, {
       username: username,
-      password: password
+      password: password,
     });
     console.log(res);
     return Promise.resolve(res.data);
@@ -39,15 +39,21 @@ export async function getUser(username: String) {
     Promise.reject(err);
   }
 }
-
+// TODO: add undefined checks for username and password
 export async function authUser(username: String, password: String) {
   try {
     const res = await axios.get(`${url}/users/`);
-    const foundUser = res.filter((user: { username: String; }) => user.username === username);
+    console.log(res);
+    const foundUser = res.data.filter(
+      (user: { username: String }) => user.username === username
+    );
     console.log("User found: ", foundUser);
 
-    const passwordsMatch = await bcrypt.compare(password, foundUser.password);
-    const usernamesMatch = foundUser.username === username;
+    const passwordsMatch = await bcrypt.compare(
+      password,
+      foundUser[0].password
+    );
+    const usernamesMatch = foundUser[0].username === username;
     if (passwordsMatch && usernamesMatch) return true;
     return false;
   } catch (err) {
