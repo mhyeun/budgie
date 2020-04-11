ad27a4cccfbd70cdce2ace7cf3225f4db86c1203
SJT00 <saadtaj96@gmail.com>
2020-04-10 23:13:49 -0400

    Chart is a seperate component now

---
+++ frontend/src/components/chart/chart.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+} from "recharts";
+import "./chart.scss";
+
+export interface ChartProps {
+  data?: any;
+}
+
+class Chart extends React.Component<ChartProps> {
+  render() {
+    return (
+      <LineChart
+        width={960}
+        height={540}
+        data={this.props.data}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="pv" stroke="#8884d8" />
+        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+      </LineChart>
+    );
+  }
+}
+
+export default Chart;
