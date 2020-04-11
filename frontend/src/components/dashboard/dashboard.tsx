41860df61d5a36cf6e014495a194dadba1e74c5e
SJT00 <saadtaj96@gmail.com>
2020-04-10 21:16:22 -0400

    recharts installed

--- frontend/src/components/dashboard/dashboard.tsx
+++ frontend/src/components/dashboard/dashboard.tsx
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
+import { LineChart, Line } from "recharts";
 import "./dashboard.scss";
+const data = [{ name: "Page A", uv: 400, pv: 2400, amt: 2400 }];
 
 const Dashboard = () => {
   return (
     <div id="dashboard">
-      <canvas id="myChart" width="400" height="400"></canvas>
+      <div id="myChart">
+        <LineChart width={400} height={400} data={data}>
+          <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        </LineChart>
+      </div>
     </div>
   );
 };
