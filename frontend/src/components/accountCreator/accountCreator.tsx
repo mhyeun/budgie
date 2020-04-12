3f4941465fd6a306af0cc23eed59e38e288cf5a0
SJT00 <saadtaj96@gmail.com>
2020-04-12 17:14:42 -0400

    account Creator

--- frontend/src/components/accountCreator/accountCreator.tsx
+++ frontend/src/components/accountCreator/accountCreator.tsx
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./accountCreator.scss";
+import history from "../../history";
+import store, { logMeIn } from "../../redux-store/store";
+import { createUser, getUser, getFinanceWithId } from "../../net";
 
 const CreateAccount = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [attemptFailed, setAttemptFailed] = useState(false);
+
+  const handleKeyPress = (e: any) => {
+    if (e.key === "Enter" && username && password) {
+      handleButtonClick(e);
+    }
+  };
+
+  const handleButtonClick = async (e: any) => {
+    setAttemptFailed(false);
+    const authorized = await createUser(username, password);
+    if (authorized) {
+      store.dispatch(
+        logMeIn(
+          authorized,
+          ((await getUser(authorized)) as any).username,
+          ((await getFinanceWithId(authorized)) as any)._id
+        )
+      );
+      history.push("/dashboard");
+    } else {
+      setAttemptFailed(true);
+    }
+  };
+
   return (
     <div id="accountCreator">
-      <div id="userCredentials"></div>
+      <div id="userCredentials">
+        <input
+          style={{
+            border: attemptFailed ? "1px solid red" : "1px solid black",
+          }}
+          placeholder="Username"
+          name="username"
+          id="user"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          onKeyPress={handleKeyPress}
+        />
+      </div>
       <div id="historyGoals"></div>
     </div>
   );
