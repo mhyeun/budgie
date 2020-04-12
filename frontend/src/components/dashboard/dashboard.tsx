bd6205c05d0340363391e015b8320db325c8cb09
mhyeun <mhyeun131@gmail.com>
2020-04-12 17:08:55 -0400

    Working chart on fashboard

--- frontend/src/components/dashboard/dashboard.tsx
+++ frontend/src/components/dashboard/dashboard.tsx
@@ -2,11 +2,29 @@ import React, { useState } from "react";
 import Chart from "../chart/chart";
 import "./dashboard.scss";
 
-import { getMockHistory } from "../../mockData";
+import { getMockHistory } from "./mockData";
 import { smallestDateInterval } from "./helper";
 
 const mockData = getMockHistory();
 const num = smallestDateInterval(mockData);
+
+console.log(mockData);
+
+const createData = (historyData: any) => {
+  const data = new Array();
+  for (let i = 0; i < historyData.length; i++) {
+    const dataPoint = {
+      name: historyData[i].date,
+      amount: historyData[i].amount,
+    };
+    data.push(dataPoint);
+  }
+  return data;
+};
+
+const data = createData(mockData);
+console.log(data);
+
 // const data = [
 //   {
 //     name: "Page A",
@@ -26,7 +44,7 @@ const Dashboard = () => {
   return (
     <div id="dashboard">
       <div id="myChart">
-        {/* <Chart data={data} /> */}
+        <Chart data={data} />
         <h1>{num}</h1>
       </div>
     </div>
